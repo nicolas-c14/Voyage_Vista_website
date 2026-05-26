5e27db81b23948a32185f1df56ab7cc251474d21
Nicolas <nicolas.copet@edu.ece.fr>
2026-05-26 18:08:56 +0200

    Ajout login admin reservations v2

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,48 +1,69 @@
 <nav class="navbar navbar-expand-lg navbar-dark custom-navbar">
+
     <div class="container">
 
-        <a class="navbar-brand fw-bold" href="index.php">
+        <a class="navbar-brand fw-bold"
+           href="/VoyageVista/index.php">
+
             VoyageVista
+
         </a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarContent">
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarContent">
+
             <span class="navbar-toggler-icon"></span>
+
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarContent">
+        <div class="collapse navbar-collapse"
+             id="navbarContent">
 
             <ul class="navbar-nav ms-auto align-items-lg-center">
 
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Accueil</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="destinations.php">Destinations</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Offres</a>
-                </li>
-
-                <!-- RESERVATIONS LINK -->
-                <?php if(isset($_SESSION["user_id"])): ?>
-
+                <!-- HOME -->
                 <li class="nav-item">
 
                     <a class="nav-link"
-                    href="/VoyageVista/reservations/my-reservations.php">
+                       href="/VoyageVista/index.php">
 
-                        Mes réservations
+                        Accueil
 
                     </a>
 
                 </li>
 
+                <!-- DESTINATIONS -->
+                <li class="nav-item">
+
+                    <a class="nav-link"
+                       href="/VoyageVista/destinations.php">
+
+                        Destinations
+
+                    </a>
+
+                </li>
+
+                <!-- RESERVATIONS -->
+                <?php if(isset($_SESSION["user_id"])): ?>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link"
+                           href="/VoyageVista/reservations/my-reservations.php">
+
+                            Mes réservations
+
+                        </a>
+
+                    </li>
+
                 <?php endif; ?>
 
-                <!-- ADMIN LINK -->
+                <!-- ADMIN -->
                 <?php if(
                     isset($_SESSION["user_role"]) &&
                     $_SESSION["user_role"] === "admin"
@@ -51,7 +72,7 @@
                     <li class="nav-item">
 
                         <a class="nav-link"
-                        href="/VoyageVista/admin/dashboard.php">
+                           href="/VoyageVista/admin/dashboard.php">
 
                             Admin
 
@@ -61,37 +82,46 @@
 
                 <?php endif; ?>
 
-                <!-- CONNEXION / DÉCONNEXION -->
+                <!-- LOGIN / LOGOUT -->
                 <?php if(isset($_SESSION["user_id"])): ?>
 
-                    <span class="text-white me-3">
+                    <li class="nav-item d-flex align-items-center">
 
-                        Bonjour
-                        <?= $_SESSION["user_name"]; ?>
+                        <span class="text-white me-3">
 
-                    </span>
+                            Bonjour
+                            <?= $_SESSION["user_name"]; ?>
 
-                    <a class="btn btn-danger"
-                    href="/VoyageVista/auth/logout.php">
+                        </span>
 
-                        Déconnexion
+                        <a class="btn btn-danger"
+                           href="/VoyageVista/auth/logout.php">
 
-                    </a>
+                            Déconnexion
+
+                        </a>
+
+                    </li>
 
                 <?php else: ?>
 
-                    <a class="btn btn-light"
-                    href="login.php">
+                    <li class="nav-item">
 
-                        Se connecter
+                        <a class="btn btn-light"
+                           href="/VoyageVista/login.php">
 
-                    </a>
+                            Se connecter
+
+                        </a>
+
+                    </li>
 
                 <?php endif; ?>
-               
+
             </ul>
 
         </div>
 
     </div>
+
 </nav>
